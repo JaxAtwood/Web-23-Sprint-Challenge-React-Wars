da09d47df276ea0f8f02b63ca0435af28188c423
Jackie Atwood <takalaj@verizon.net>
2019-09-13 14:04:18 -0400

    MVP met, rendering, styled

---
+++ starwars/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1>
+                SWAPI DATA:
+            </h1>
+        </div>
+    )
+}
+
+export default Header;
